6e09f9c1216c8342de9d07826901a6390ef4c6e6
endalough <loughman.enda@gmail.com>
2024-11-12 20:42:18 +0000

    Basic, unstructured working example

---
+++ app/database.py
@@ -0,0 +1,21 @@
+from sqlmodel import create_engine, SQLModel, Session
+from fastapi import Depends
+from typing import Annotated
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+SQLModel.metadata.drop_all(engine)
+SQLModel.metadata.create_all(engine)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlmodel import Field, SQLModel
+
+class ItemBase(SQLModel):
+    name: str | None = Field(default=None, index=True)
+    location: str | None = Field(default=None, index=True)
+    sub_location: str | None =None
+    description: str | None = None
+    quantity: int | None = None
+    
+class Item(ItemBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class ItemOptionals(ItemBase):
+    name: str | None = None
+    location: str | None = None
+    sub_location: str | None = None
+    description: str | None = None
+    quantity: int | None = None
+
+def all_attributes_none(model: SQLModel) -> bool:
+    return all(value is None for value in model.model_dump(exclude_unset=True).values())
